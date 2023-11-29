825e95cdeb42fce3259125b932378fa151bdeba6
Santiago <san99ig@gmail.com>
2023-11-29 20:40:52 -0300

    fixes

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,10 +1,11 @@
 import Post from "../models/Post.js";
+import User from "../models/User.js";
 
 export const createPost = async (req, res) => {
     try {
         const { userId, description, picturePath } = req.body;
-        const user = await User.findbyId(userId);
-        const nerPost = new Post({
+        const user = await User.findById(userId);
+        const newPost = new Post({
             userId,
             firstName: user.firstName,
             lastName: user.lastName,
@@ -18,7 +19,7 @@ export const createPost = async (req, res) => {
         await newPost.save();
 
 
-        const post = await Post.find();
+        const post = await Post.find().sort({ createdAt: -1 });
         res.status(201).json(post);
     } catch (err) {
         res.status(409).json({ message: err.message})
@@ -27,7 +28,7 @@ export const createPost = async (req, res) => {
 
 export const getFeedsPosts = async (req, res) => {
     try {
-        const post = await Post.find();
+        const post = await Post.find().sort({ createdAt: -1 });
         res.status(200).json(post);
     } catch (err) {
         res.status(404).json({ message: err.message})
@@ -37,7 +38,7 @@ export const getFeedsPosts = async (req, res) => {
 export const getUserPosts = async (req, res) => {
     try {
         const { userId } = req.params;
-        const post = await Post.find();
+        const post = await Post.find({ userId }).sort({ createdAt : -1 });
         res.status(200).json(post);
     } catch (err) {
         res.status(404).json({ message: err.message})
@@ -48,7 +49,7 @@ export const likePost = async (req, res) => {
     try {
         const { id } = req.params;
         const { userId } = req.body;
-        const post = await Post.findbyId(id);
+        const post = await Post.findById(id);
         const isLiked = post.likes.get(userId);
 
         if (isLiked) {
@@ -57,7 +58,7 @@ export const likePost = async (req, res) => {
             post.likes.set(userId, true);
         }
 
-        const updatedPost = await Post.findbyIdAndUpdate(
+        const updatedPost = await Post.findByIdAndUpdate(
             id,
             { likes: post.likes },
             { new: true },
